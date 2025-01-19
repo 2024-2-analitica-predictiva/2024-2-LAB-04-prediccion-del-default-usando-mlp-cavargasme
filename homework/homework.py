c8464fe946f11a6eecda6c3b8bc353ca3e85bac9
cavargasme <cavargasme@unal.edu.co>
2025-01-19 15:41:54 -0500

    Carga_Lab04

--- homework/homework.py
+++ homework/homework.py
@@ -208,7 +208,7 @@ def optimize_hyperparameters(pipeline, x_train, y_train):
     param_grid = {
         'pca__n_components': [None],
         'select_k_best__k': [20],
-        'model__hidden_layer_sizes': [(51, 31, 41)],
+        'model__hidden_layer_sizes': [(50, 30, 40, 60)],
         # 'model__activation': ['relu'],
         # 'model__solver': ['adam'],
         'model__alpha': [0.26],
@@ -216,7 +216,7 @@ def optimize_hyperparameters(pipeline, x_train, y_train):
         'model__learning_rate_init': [0.001],
     }
     
-    grid_search=GridSearchCV(pipeline, param_grid, cv=10, scoring='balanced_accuracy', n_jobs=-1)
+    grid_search=GridSearchCV(pipeline, param_grid, cv=10, scoring='balanced_accuracy', n_jobs=-1, verbose = 1, refit=True)
     grid_search.fit(x_train, y_train)
     
     with mlflow.start_run():
